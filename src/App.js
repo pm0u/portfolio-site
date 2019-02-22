9e53f5630df4c9181f169c121b656dfa1b5d63c8
Paul Mourer <paul.mourer@gmail.com>
2019-02-22 14:02:30 -0700

    added coming soon placeholder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
       <>
         <div className='window-shadowed'>
           <div className='window-inner'>
-          <h1 className='window-heading'>paul mourer's portfolio</h1>
+            <h1 className='window-heading'>paul mourer's portfolio</h1>
+            <p className='window-text-centered'>Coming soon...</p>
           </div>
         </div>
       </>
