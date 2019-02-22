9e746bff133d94562a201fa819327698111f7ab5
Paul Mourer <paul.mourer@gmail.com>
2019-02-22 14:00:42 -0700

    basic window showing up centered, going with default monospace font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './index.css'
 
 class App extends Component {
   render() {
     return (
       <>
-      <p>test</p>
-      <p>test2</p>
+        <div className='window-shadowed'>
+          <div className='window-inner'>
+          <h1 className='window-heading'>paul mourer's portfolio</h1>
+          </div>
+        </div>
       </>
     )
   }
